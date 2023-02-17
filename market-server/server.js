a63862a0b3271b9e12d289aaf7ff8370857c3eb6
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-17 12:04:13 +0900

    Update server.js

--- market-server/server.js
+++ market-server/server.js
@@ -7,9 +7,13 @@ const app = express();
 const models = require("./models");
 const port = 8090;
 
+// express에 대해 설정
+// 서버(express)에서 json 형식의 데이터를 처리할 수 있도록 해준다
 app.use(express.json());
+// 모든 브라우저에서 내가 만든 서버에 요청할 수 있다.
 app.use(cors());
 
+// 이 경로로(/products)method가 get인 요청이 왔을 때  두번째 인자 arrow function 실행
 app.get("/products", (req, res) => {
   models.Product.findAll()
     .then((result) => {
@@ -23,7 +27,9 @@ app.get("/products", (req, res) => {
 
 // body
 
-// JS에서 객체를 표현할 때 ES6에서는 key와 value가 똑같다
+// body에 데이터를 담는 부분
+// JS에서 객체를 표현할 때 ES6에서는 key와 value가 똑같다면
+// 그때 생략 가능(body: body -> body)
 app.post("/products", (req, res) => {
   const body = req.body;
   const { name, price, seller, description } = body;
@@ -58,7 +64,7 @@ app.get("/products/:id/events/:eventId", (req, res) => {
 // app.listen을 통해서 서버가 실행
 // 정상적으로 실행됬을때 두번째 인자의 callback 함수가 실행
 app.listen(port, () => {
-  console.log("그랩의 쇼핑몰 서버가 실행 중 입니다");
+  console.log("쇼핑몰 서버가 실행 중 입니다");
   // 실행됬을 때 동기화하는 작업(.sync)
   // (models파일로 생성된 sequrlize(index.js)를 불러온다)
   // index.js에서 상품과 관련된 테이블을 만들면 데이터베이스에서 상품과 관련되 테이블을 만든다
